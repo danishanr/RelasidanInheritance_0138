5f79e7a9f25d3f601e085d7033e1761b2b02f054
danishanr <danishaanr@gmail.com>
2025-05-27 21:12:32 +0700

    Membuat relasi agregasi ibu

---
+++ ibu2.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#ifndef IBU_H
+#define IBU_H
+
+class anak;  
+
+class Ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    Ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
+void Ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void Ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
+#endif
