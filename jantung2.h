8112f9a7c17cff32987791b2c05882004d831f24
danishanr <danishaanr@gmail.com>
2025-05-27 21:22:22 +0700

    Mendefinisikan jantung untuk relasi komposisi

---
+++ jantung2.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public :
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
